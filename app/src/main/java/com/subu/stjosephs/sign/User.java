149be35bdb3ce436ca700d506dc2bb97b14823bc
suresh <subramanianganesan162@gmail.com>
2018-12-25 11:28:18 +0530

    Toll gate login and push data

---
+++ app/src/main/java/com/subu/stjosephs/sign/User.java
@@ -0,0 +1,16 @@
+package com.subu.stjosephs.sign;
+
+public class User
+{
+    public String name, email, phone;
+
+    public User(){
+
+    }
+
+    public User(String name, String email, String phone) {
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+    }
+}
