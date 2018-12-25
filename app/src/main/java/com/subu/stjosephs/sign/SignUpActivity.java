87203d10b43fdd66a661ec07dff8b1884c72c7e5
suresh <subramanianganesan162@gmail.com>
2018-12-25 16:58:12 +0530

    Toll gate login and push data

--- app/src/main/java/com/subu/stjosephs/sign/SignUpActivity.java
+++ app/src/main/java/com/subu/stjosephs/sign/SignUpActivity.java
@@ -3,11 +3,11 @@ package com.subu.stjosephs.sign;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Main2Activity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_sign_up);
     }
 }
